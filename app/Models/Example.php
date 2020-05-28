8fd178d612eef6ae188527cf573b44c9431e28fa
Anamcoollzz <hairulanam21@gmail.com>
2020-05-28 19:51:23 +0700

    initial commit

---
+++ app/Models/Example.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Log extends Model {
+
+    protected $fillable = [
+
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    protected $dates = [
+
+    ];
+
+}
